944fe47b1fd86539fecc7401defe8fbcccbca0aa
Angie Gove <angie.gove@betterbills.com>
2017-01-24 18:12:42 +1100

    Add progress route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/students/:id' => 'students#show'
+  get '/students/:id/progress' => 'students#progress'
 end
--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "routing to student", :type => :routing do
-  context "student" do
-    it "routes /students/:id to students#show" do
-       expect(get: "/students/1").to route_to(
-         controller: "students",
-         action: "show",
-         id: "1"
-         )
+  describe "student" do
+    context "#show" do
+      it "routes /students/:id to students#show" do
+         expect(get: "/students/1").to route_to(
+           controller: "students",
+           action: "show",
+           id: "1"
+           )
+      end
     end
+    context "#progress" do
+      it "routes /students/:id/progress to students#progress" do
+         expect(get: "/students/1/progress").to route_to(
+           controller: "students",
+           action: "progress",
+           id: "1"
+          )
+      end
+    end
+
   end
 end
