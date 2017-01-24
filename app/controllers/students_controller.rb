c3f37bf2b01c7638f9e7e1333ab3cc197c087023
Angie Gove <angie.gove@betterbills.com>
2017-01-24 18:13:33 +1100

    student progress action should return a son response of the lesson and part details

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
     render json: @student
   end
+
+  def progress
+    @student = Student.find(params[:id])
+    @part = Part.find(@student.progress.lesson_id)
+    @lesson = Lesson.find(@student.progress.lesson_id)
+    render json: { student: @student.username,
+                   lesson: @lesson.number,
+                   part: @part.number
+                 }
+  end
 end
---
+++ spec/controllers/students_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe StudentsController do
+
+  describe '#show' do
+    context 'student is found ' do
+      it "finds student by username" do
+        student = Student.create!
+        get :show, params: {id: student.id}
+        parsed_response = JSON.parse(response.body)
+        expect(response.body).to eq(student.to_json)
+      end
+    end
+  end
+
+  describe '#progress' do
+    context 'student is found ' do
+      it "returns students progress as json " do
+        student = Student.create(username: "bottle", name: 'go', surname: 'bottle')
+        lesson = Lesson.create(number:1)
+        part = Part.create(number:2, lesson_id: lesson.id)
+        progress = Progress.create(lesson_id:lesson.id, part_id: part.id, student_id: student.id)
+
+        get :progress, params: {id: student.id}
+
+        expect(response.body).to include({student:"bottle",lesson:1,part:2}.to_json)
+
+
+      end
+    end
+
+    context 'student is not found ' do
+      it 'raises a 404' do
+        expect { get '/student/notastudent/progress' }.to raise_error(ActionController::UrlGenerationError)
+      end
+    end
+  end
+end
